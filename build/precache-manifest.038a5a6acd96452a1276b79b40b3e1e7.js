606016b61b9d65df2528862b6adfed812c53dc68
ardanclassic <ardan.classic@gmail.com>
2020-04-13 13:29:09 +0700

    add build

---
+++ build/precache-manifest.038a5a6acd96452a1276b79b40b3e1e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840f6257f328cc4f0e4938833ac730e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ea7de8366ec993a930c",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "3b5f0c13492804b11831",
+    "url": "/static/css/main.ee732f24.chunk.css"
+  },
+  {
+    "revision": "0ea7de8366ec993a930c",
+    "url": "/static/js/2.9e959ac0.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/static/js/2.9e959ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5f0c13492804b11831",
+    "url": "/static/js/main.bf699845.chunk.js"
+  },
+  {
+    "revision": "1ebe9c42b40ceb0a717c",
+    "url": "/static/js/runtime-main.38ca9329.js"
+  }
+]);
